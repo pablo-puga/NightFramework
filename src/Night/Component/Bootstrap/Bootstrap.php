3b34511582f5102d3e58757d1598586c0306b932
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-15 23:28:11 +0100

    Modified the Bootstrap class to call the necessary controller associated to a specific route

--- src/Night/Component/Bootstrap/Bootstrap.php
+++ src/Night/Component/Bootstrap/Bootstrap.php
@@ -3,6 +3,8 @@
 namespace Night\Component\Bootstrap;
 
 
+use Night\Component\Routing\Routing;
+
 class Bootstrap
 {
     const NIGHT_PRODUCTION_ENVIRONMENT = 'prod';
@@ -10,11 +12,22 @@ class Bootstrap
 
     private $currentEnvironment;
     private $configurationsDirectory;
+    private $fileParser;
 
-    public function __construct($environment, $configurationsDirectory)
+    public function __construct($environment, $configurationsDirectory, $fileParser)
     {
-        $this->currentEnvironment = $environment;
+        $this->currentEnvironment      = $environment;
         $this->configurationsDirectory = $configurationsDirectory;
+        $this->fileParser              = $fileParser;
+    }
+
+    public function __invoke($route)
+    {
+        $routing = new Routing($this->configurationsDirectory, $this->fileParser);
+        $controllerInfo = $routing->parseRoute($route);
+
+        $controller = new $controllerInfo['namespace']();
+        $controller->{$controllerInfo['method']}();
     }
 }
 
