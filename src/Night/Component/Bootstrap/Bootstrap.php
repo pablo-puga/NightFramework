512c1acff5b541950904728a9790fd41abb12401
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-16 01:22:31 +0100

    The Bootstrap now selectes what FileParser to instantiate between the YAMLParser and the JSONParser

--- src/Night/Component/Bootstrap/Bootstrap.php
+++ src/Night/Component/Bootstrap/Bootstrap.php
@@ -3,6 +3,7 @@
 namespace Night\Component\Bootstrap;
 
 use Night\Component\FileParser\YAMLParser;
+use Night\Component\FileParser\JSONParser;
 use Night\Component\Routing\RouteControllerInformation;
 use Night\Component\Routing\Routing;
 use Symfony\Component\Yaml\Yaml;
@@ -11,10 +12,7 @@ class Bootstrap
 {
     const NIGHT_PRODUCTION_ENVIRONMENT = 'prod';
     const NIGHT_DEVELOPMENT_ENVIRONMENT = 'dev';
-    const NIGHT_YAML_FILE_EXTENSION = 'yml';
-    const NIGHT_PHP_FILE_EXTENSION = 'php';
-    const NIGHT_JSON_FILE_EXTENSION = 'json';
-
+    
     private $generalConfigurations;
 
     public function __construct(Array $generalConfigurations)
@@ -25,18 +23,17 @@ class Bootstrap
     public function __invoke($route)
     {
         switch ($this->generalConfigurations['configurationsFileExtension']) {
-            case self::NIGHT_PHP_FILE_EXTENSION:
-                $fileParser = null;
+            case JSONParser::FILE_EXTENSION:
+                $fileParser = new JSONParser();
                 break;
-            case self::NIGHT_JSON_FILE_EXTENSION:
-                $fileParser = null;
-                break;
-            case self::NIGHT_YAML_FILE_EXTENSION:
+            case YAMLParser::FILE_EXTENSION:
             default:
                 $fileParser = new YAMLParser(new Yaml());
         }
 
-        $routing    = new Routing($this->generalConfigurations['configurationsDirectory'], $fileParser);
+        $configurationsDirectory     = $this->generalConfigurations['configurationsDirectory'];
+        $configurationsFileExtension = $this->generalConfigurations['configurationsFileExtension'];
+        $routing                     = new Routing($configurationsDirectory, $configurationsFileExtension, $fileParser);
 
         $routeControllerInformation = $routing->parseRoute($route);
         $this->invokeController($routeControllerInformation);
