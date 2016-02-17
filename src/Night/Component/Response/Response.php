a4b191056edc972f3374e4288b3890d59c68d194
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-17 21:49:10 +0100

    Created part of the Response component

---
+++ src/Night/Component/Response/Response.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Night\Component\Response;
+
+
+abstract class Response
+{
+    const MOVED_PERMANENTLY = 301;
+    const SEE_OTHER = 303;
+    const TEMPORARY_REDIRECT = 307;
+
+    protected $headers = array();
+
+    protected function setContentType($contentType)
+    {
+        $this->headers['Content-type:'] = $contentType;
+    }
+
+    protected function setResponseStatus($status, $message)
+    {
+        $this->headers['HTTP/1.1'] = "$status $message";
+    }
+
+    protected function redirect($destinationURL, $redirectCode)
+    {
+        switch ($redirectCode) {
+            case self::MOVED_PERMANENTLY:
+                break;
+            case self::SEE_OTHER:
+                break;
+            case self::TEMPORARY_REDIRECT:
+                break;
+            default:
+
+        }
+        $this->headers['Location:'] = $destinationURL;
+    }
+}
+
+
+
+
