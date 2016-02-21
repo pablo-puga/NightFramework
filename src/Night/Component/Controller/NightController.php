b6cc25b2dce6918a180c4e1623b6e9faef23083e
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-21 18:25:36 +0100

    Included the service Templating into the abstract class NightController

--- src/Night/Component/Controller/NightController.php
+++ src/Night/Component/Controller/NightController.php
@@ -3,8 +3,16 @@
 namespace Night\Component\Controller;
 
 
+use Night\Component\Templating\Templating;
+
 abstract class NightController
 {
+    /** @var  Templating */
+    protected $templating;
 
+    public function setTemplating(Templating $templating)
+    {
+        $this->templating = $templating;
+    }
 }
 
