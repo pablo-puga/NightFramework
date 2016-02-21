aa71b5127ac2601f791d4d27e399f5bfc6683bed
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-21 17:57:30 +0100

    Created the exceptionb UnknownTemplatingEngine

---
+++ src/Night/Component/Templating/Exception/UnknownTemplatingEngine.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Night\Component\Templating\Exception;
+
+
+class UnknownTemplatingEngine extends \InvalidArgumentException
+{
+    public function __construct($message, $code, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public static function throwDefault($engine)
+    {
+        throw new self("Unknown templating engine [$engine]", 1);
+    }
+}
+
