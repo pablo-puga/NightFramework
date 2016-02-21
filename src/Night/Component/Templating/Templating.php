3aca9384c1b531404c62ceac41efc45e53fc81aa
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-21 18:25:05 +0100

    Added the setTemplate method to the Templating interface

--- src/Night/Component/Templating/Templating.php
+++ src/Night/Component/Templating/Templating.php
@@ -6,6 +6,7 @@ namespace Night\Component\Templating;
 interface Templating
 {
     public function setVariable($variable, $value);
+    public function setTemplate($template);
     public function render();
 }
 
