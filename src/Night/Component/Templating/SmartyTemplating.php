acbf7c9722f7c9caaea23773dbd146b8e9f9c0b8
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-25 16:16:39 +0100

    The SmartyTemplating component supports now setting the cache and compilation settings through the general.yml file

--- src/Night/Component/Templating/SmartyTemplating.php
+++ src/Night/Component/Templating/SmartyTemplating.php
@@ -20,7 +20,15 @@ class SmartyTemplating implements Templating
         $generalConfigurationFile = '../' . Bootstrap::CONFIGURATIONS_DIRECTORY . '/general.yml';
         $smartySettings           = $fileParser->parseFile($generalConfigurationFile)['templating']['smarty'];
         $smarty                   = new Smarty();
-        $smarty->setTemplateDir($smartySettings['templatesDirectory']);
+        $smarty->setTemplateDir($smartySettings['templates']['directory']);
+        if (isset($smartySettings['compilation']['directory']) && !empty($smartySettings['compilation']['directory'])) {
+            $smarty->setCompileDir($smartySettings['compilation']['directory']);
+        }
+        if ($smartySettings['cache']['enable']) {
+            $smarty->setCacheDir($smartySettings['cache']['directory']);
+            $smarty->setCaching(Smarty::CACHING_LIFETIME_CURRENT);
+            $smarty->setCacheLifetime($smartySettings['cache']['lifeTime']);
+        }
         $this->smarty = $smarty;
     }
 
