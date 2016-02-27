05c717e89a4690f1257d603492d9b10933ac01bc
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-27 18:42:22 +0100

    Created the RoutingProfiler class

---
+++ src/Night/Component/Profiling/RoutingProfiler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Night\Component\Profiling;
+
+
+use Night\Component\Request\Route;
+
+class RoutingProfiler extends ProfilerComponent
+{
+    private $requestedUri;
+    /** @var  Route */
+    private $matchedDefinition;
+    private $parsingTime;
+    private $routeArguments;
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function setInformation($requestedUri, $matchedDefinition, Array $routeArguments, $parsingTime)
+    {
+        $this->requestedUri      = $requestedUri;
+        $this->matchedDefinition = $matchedDefinition;
+        $this->parsingTime       = $parsingTime;
+        $this->routeArguments    = $routeArguments;
+    }
+
+    public function getProfilingData()
+    {
+        $parameters = "";
+        foreach ($this->routeArguments as $routeArgument => $value) {
+            $parameters .= "[$routeArgument]: $value<br>";
+        }
+        if (empty($parameters)) $parameters = 'None';
+        $html = <<<__HTML__
+<table>
+    <tr class="table-title"><th colspan="2" style="text-align: center">ROUTING PROFILING INFORMATION</th></tr>
+    <tr class="empty"><td colspan="2"></td></tr>
+    <tr><td>Requested URI:</td><td>$this->requestedUri</td></tr>
+    <tr><td>Matched Route:</td><td>$this->matchedDefinition</td></tr>
+    <tr><td>Parameters: </td><td>$parameters</td></tr>
+    <tr><td>Parsing Duration: </td><td>$this->parsingTime</td>
+</table>
+__HTML__;
+        return $html;
+    }
+}
+
