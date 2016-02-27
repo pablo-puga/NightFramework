d7c1e5416f07d20c25e6d1ada7ad69079c939813
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-27 18:40:40 +0100

    The PDORepositoryProfiler implements now the function getInstance

--- src/Night/Component/Profiling/PDORepositoryProfiler.php
+++ src/Night/Component/Profiling/PDORepositoryProfiler.php
@@ -8,6 +8,15 @@ use ReflectionClass;
 final class PDORepositoryProfiler extends ProfilerComponent
 {
     private $traces = [];
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 
     public function addTrace($statement, Array $variables, $result, $error)
     {
