cb02f1d8dbd387d826b0acb86cbb72dd3eb3d629
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-27 18:40:09 +0100

    Abstract class ProfilerComponent implements now the ProfilerComponentInterface

--- src/Night/Component/Profiling/ProfilerComponent.php
+++ src/Night/Component/Profiling/ProfilerComponent.php
@@ -3,21 +3,9 @@
 namespace Night\Component\Profiling;
 
 
-abstract class ProfilerComponent
+abstract class ProfilerComponent implements ProfilerComponentInterface
 {
-    protected static $instance = null;
-
     protected function __construct() {}
     protected function __clone() {}
-
-    public static function getInstance()
-    {
-        if (is_null(self::$instance)) {
-            self::$instance = new static();
-        }
-        return self::$instance;
-    }
-
-    abstract public function getProfilingData();
 }
 
